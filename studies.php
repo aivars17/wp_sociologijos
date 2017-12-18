4c891b4e136dcd489baa763cf1c0ee0a28571c3e
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2017-12-18 10:10:26 +0200

    header and footer
    changed files html to php

---
+++ studies.php
@@ -0,0 +1,138 @@
+<?php get_header('single'); ?>
+
+    <section class="study">
+
+      <div class="Container">
+        <!-- <div class="sectionHead disabled1">
+          <img style="" src="slices/slice11.png" alt="">
+          <h1>STUDIJOS</h1>
+        </div> -->
+<!--
+      <div class="sectionHead-vertical disabled">
+        <h1>STUDIJOS<img style="margin-left:1%;" src="slices/slice5.png" alt=""></h1>
+
+      </div> -->
+      <div class=" sectionHead-vertical disabled">
+        <h1 class="disabled">STUDIJOS<img src="slices/slice5.png" alt=""></h1>
+
+      </div>
+
+   <h2 class="h4Heading">Universitetai</h2>
+
+
+
+
+      <div class="studyContainer">
+
+
+
+        <div class="studyContainer-item">
+          <img src="assets/img/vu1.png" alt="">
+
+        </div>
+        <div class="studyContainer-item">
+          <img src="assets/img/vu2.png" alt="">
+
+        </div>
+        <div class="studyContainer-item">
+          <img src="assets/img/vu3.png" alt="">
+
+
+        </div>
+      </div>
+
+    </div>
+    </section>
+
+    <section class="grades">
+      <div class="Container">
+        <h4 class="h4Heading">Stojimu balai</h4>
+<div class="gradesContainer">
+  <div class="gradesContainer-item firstTable">
+    <table>
+  <tr>
+    <th colspan="2">2017</th>
+  </tr>
+  <tr>
+    <td>Istorija</td>
+    <td class="tdWhite">0.4</td>
+  </tr>
+  <tr>
+    <td>Lietuviu Kalba</td>
+    <td class="tdWhite">0.2</td>
+  </tr>
+  <tr>
+    <td>Matematika ir IT</td>
+    <td class="tdDark">0.2</td>
+  </tr>
+  <tr>
+    <td>Bet kuris kitas dalykas</td>
+    <td class="tdDark">0.2</td>
+  </tr>
+</table>
+  </div>
+
+  <div class="gradesContainer-item">
+    <table>
+  <tr>
+    <th colspan="2">2018</th>
+  </tr>
+  <tr>
+    <td>Matematika</td>
+    <td class="tdWhite">0.4</td>
+  </tr>
+  <tr>
+    <td>Lietuviu Kalba</td>
+    <td class="tdWhite">0.2</td>
+  </tr>
+  <tr>
+    <td>Istorija</td>
+    <td class="tdDark">0.2</td>
+  </tr>
+  <tr>
+    <td>Bet kuris kitas dalykas</td>
+    <td class="tdDark">0.2</td>
+  </tr>
+</table>
+  </div>
+
+  <div class="gradesContainer-item lastTable">
+    <table>
+
+  <tr>
+    <td><div class="tdWhite"></div></td>
+    <td>- Egzaminai</td>
+  </tr>
+  <tr>
+    <td><div class="tdDark"></div></td>
+    <td>- Egzaminai arba metiniai pazymiai</td>
+  </tr>
+</table>
+  </div>
+
+
+
+</div>
+<p class="paskaiciuok"><a href="#">Paskaiciuok savo bala</a></p>
+      </div>
+    </section>
+
+    <section class="contactForm">
+      <div class="Container">
+        <div class="contactFormContainer">
+          <h4>Turi klausimu? Klausk!</h4>
+
+          <label for="">Vardas</label>
+          <input type="text" name="" value="">
+          <label for="">El.pastas</label>
+          <input type="text" name="" value="">
+          <label for="">Klausimas</label>
+          <textarea name="name" rows="8" cols="80"></textarea>
+          <button id="formButton" type="button" name="button">Siusti</button>
+          <div class="clearFix"></div>
+        </div>
+
+      </div>
+
+    </section>
+<?php get_footer(); ?>
